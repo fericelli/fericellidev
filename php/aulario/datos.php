757c12174e8d32bb236c47cfec1c6fc88d444a76
fericelli <fericelli.garcia@gmail.com>
2024-04-16 10:37:49 -0400

    aa

--- php/aulario/datos.php
+++ php/aulario/datos.php
@@ -4,10 +4,37 @@
 		
 		
 		function __construct(){
-           
+            include("../conexion.php");
+			$this->Conexion = new Conexion();
+			echo "[".$this->retorno()."]";
+			$this->Conexion->CerrarConexion();
 		}
 		private function retorno(){
-			
+			if(isset($_POST["materia"])){
+				$consutar = $this->Conexion->Consultar("SELECT * FROM horarios LEFT JOIN materias ON materias.codigo=horarios.materia AND materias.carrera=horarios.carrera WHERE seccion IS NOT NULL AND profesor IS NOT NULL AND aula IS NOT NULL AND entrada IS NOT NULL AND salida IS NOT NULL AND sede IS NOT NULL AND jornada IS NOT NULL AND nombre LIKE '".$_POST["materia"]."%' ORDER BY entrada ASC");
+             
+			}else  if(isset($_POST["registros"])){
+				$consutar = $this->Conexion->Consultar("SELECT * FROM horarios LEFT JOIN materias ON materias.codigo=horarios.materia AND materias.carrera=horarios.carrera LEFT JOIN pensum ON pensum.codigo=horarios.materia AND pensum.carrera=horarios.carrera ORDER BY semestre ASC");
+             
+			}else if(isset($_POST["materiaadmi"])){
+				$consutar = $this->Conexion->Consultar("SELECT * FROM horarios LEFT JOIN materias ON materias.codigo=horarios.materia AND materias.carrera=horarios.carrera LEFT JOIN pensum ON pensum.codigo=horarios.materia AND pensum.carrera=horarios.carrera WHERE materias.nombre LIKE '".$_POST["materiaadmi"]."%' ORDER BY semestre ASC");
+			}else{
+				$consutar = $this->Conexion->Consultar("SELECT * FROM horarios LEFT JOIN materias ON materias.codigo=horarios.materia AND materias.carrera=horarios.carrera WHERE seccion IS NOT NULL AND profesor IS NOT NULL AND aula IS NOT NULL AND entrada IS NOT NULL AND salida IS NOT NULL AND sede IS NOT NULL AND jornada IS NOT NULL ORDER BY entrada ASC");
+             
+			}
+			$retorno = "";
+			while($horario = $this->Conexion->Recorrido($consutar)){
+				$curso = $horario[11];
+				$profesor = $horario[2];
+				$seccion = $horario[0];
+				$aula = $horario[3];
+ 				$horaentrada = $horario[4];
+				$horasalida = $horario[5];
+				$codigo = $horario[1];
+				$dia = $this->dia($horario[9]);
+				$retorno .= '["'.$curso.'","'.$profesor.'","'.$seccion.'","'.$aula.'","'.$horaentrada.'","'.$horasalida.'","'.$dia.'","'.$codigo.'"],';
+			}
+			return substr($retorno,0,strlen($retorno)-1);
         }
 		private function dia($opcion){
 			switch ($opcion) {
