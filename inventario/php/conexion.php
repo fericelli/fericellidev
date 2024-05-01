2d401e3d3ab1eb5ab94a1122362d359a2e52f3f4
fericelli <fericelli.garcia@gmail.com>
2024-05-01 15:00:17 -0400

    a

--- inventario/php/conexion.php
+++ inventario/php/conexion.php
@@ -7,7 +7,7 @@
 		private $Conectar;
 		
 		function __construct(){
-			$this->Conexiones("localhost","u956446715_wilfredo","K$G355#Y4+k","u956446715_inventario");
+			$this->Conexiones("https://fericellidev.com","u956446715_wilfredo","K$G355#Y4+k","u956446715_inventario");
 		}
 		private function Conexiones($S,$U,$C,$B){
 			$this->Servidor = $S;
