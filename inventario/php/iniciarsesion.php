0ce997fbc8d3e6bdb38195294e582611921cd22a
fericelli <fericelli.garcia@gmail.com>
2024-05-01 14:55:50 -0400

    a

--- inventario/php/iniciarsesion.php
+++ inventario/php/iniciarsesion.php
@@ -8,30 +8,7 @@
 			$this->Conexion->CerrarConexion();
 		}
         private function retorno(){
-            // return var_dump($_POST);
-             $consultar = $this->Conexion->Consultar("SELECT * FROM usuarios WHERE usuario='".$_POST["usuario"]."' OR correo='".$_POST["usuario"]."'");
-             $retorno = "[";
-             $correo = "";
-            if($this->Conexion->Recorrido($consultar)){
-                
-            }else{
-                $retorno .= '"Usuario no registrado","error"';
-            }
-            $consultar2 = $this->Conexion->Consultar("SELECT * FROM usuarios WHERE usuario='".$_POST["usuario"]."' AND clave='".$_POST["clave"]."'");
             
-            if(strlen($retorno)==1){
-                if($this->Conexion->Recorrido($consultar2)){
-                }else{
-                    $retorno .= '"Contraseña incorrecta","error"';
-                }
-            }
-
-            if(strlen($retorno)==1){
-                $retorno .= '"Iniciando sesión","correcto","inventario"';
-            }
-             
- 
-            return $retorno."]";
         } 
     }
     new IniciarSesion();
